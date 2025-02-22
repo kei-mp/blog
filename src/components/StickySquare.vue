80bf1f8f7bc64f96c516216093dfc41180d6e31a
kei-mp <52072305+kei-mp@users.noreply.github.com>
2025-02-22 17:40:34 +0200

    layout: add sticky square for graph

---
+++ src/components/StickySquare.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sticky-square"></div>
+</template>
+
+<style scoped>
+.sticky-square {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  position: sticky;
+  top: 1em;
+  float: right;
+  margin-right: -150px;
+  /* Ensure the square stays above content */
+  z-index: 10;
+}
+</style> 
