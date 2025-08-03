638688946be42e898c3d2cc8eb56ca28e520d0e9
kei-mp <52072305+kei-mp@users.noreply.github.com>
2025-08-03 06:33:42 -0500

    fix: remove home link in nav bar since users can click title - would cause other links to wrap on mobile

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Kei's Blog";
+export const SITE_TITLE = "keiBlog";
 export const SITE_DESCRIPTION = "What I'm thinking about";
